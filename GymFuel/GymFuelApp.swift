c1d4bf4395a4e7d2a87269bfc059df76d8695377
Ahmad Ali Tariq <ahmed.ali.tariq@msn.com>
2025-12-06 16:26:13 +0500

    Initial Commit

---
+++ GymFuel/GymFuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymFuelApp.swift
+//  GymFuel
+//
+//  Created by Ahmad Ali Tariq on 06/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct GymFuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
